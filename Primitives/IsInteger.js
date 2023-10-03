8a22e393cb7cc5571bb910f3ad12c43f97c917c6
ShwetaPal <shwetapal975@gmail.com>
2023-10-03 23:39:05 +0530

    random

---
+++ Primitives/IsInteger.js
@@ -0,0 +1,19 @@
+// 2. Write a function which returns true if given value of number is an integer without using any inbuilt functions
+
+function isInt(val) {
+  if (val % 1 == 0) return true;
+  return false;
+}
+
+console.log(isInt(2.03)); //false
+console.log(isInt(2.0)); //true
+console.log(isInt(5)); //true
+console.log(isInt(55)); //true
+
+// Create a function which returns a random number in the given range of values both inclusive
+function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
+  return rangeStart + Math.round(Math.random() * (rangeEnd - rangeStart));
+}
+
+const randomnum = randomNumberGeneratorInRange(10, 50);
+console.log(randomnum);
