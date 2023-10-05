badac73ce370ebc1ab151d0413745d1778811ecd
ShwetaPal <shwetapal975@gmail.com>
2023-10-05 22:47:46 +0530

    Output Day-5

---
+++ Output_Day5.js
@@ -0,0 +1,12 @@
+function changeProperty(person) {
+  person.age = 25;
+  person = {
+    name: "John",
+    age: 50,
+  };
+  return person;
+}
+const personObj1 = { name: "Alex", age: 30 };
+const personObj2 = changeProperty(personObj1);
+console.log(personObj1); //{ name: 'Alex', age: 25 }
+console.log(personObj2); //{ name: 'John', age: 50 }
